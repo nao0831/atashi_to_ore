2f48ff7cfa7f4a8791f35dcad74e51310e1d5fb1
Nao <nao.m.0831@gmail.com>
2012-12-15 00:33:49 +0900

    use git

---
+++ app/models/article.rb
@@ -0,0 +1,3 @@
+class Article < ActiveRecord::Base
+  attr_accessible :memo, :title, :url
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atashitoore::Application.config.secret_token = 'b5065a1eb010e3510e3bf0c2f42b4493a3a817d448b9939cd444766561484bec385b6bd26b030409939a47fbfb40f40dbd4153706c5cca74c1d17642b3e1a873'
