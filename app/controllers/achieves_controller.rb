3b4f85c4e0f634226d303f2c357bf7211fe365e0
Nao Maemura <nao.m.0831@gmail.com>
2013-03-14 18:52:57 +0900

    create achieve session

--- app/controllers/achieves_controller.rb
+++ app/controllers/achieves_controller.rb
@@ -12,4 +12,40 @@ class AchievesController < ApplicationController
 
 	end
 
+
+	def new
+		@achieve = Achieve.new
+	end
+
+	def create
+		@achieve = Achieve.new(params[:achieve])
+		if @achieve.save
+			redirect_to achieves_path, notice: "成功したよん"
+		else
+			#newのviewをもう一回書きなおす
+			render action: "new"
+		end
+
+	end
+
+	def edit
+		@achieve = Achieve.find(params[:id])
+	end
+
+	def update
+		@achieve = Achieve.find(params[:id])
+		if @achieve.update_attributes(params[:achieve])
+			redirect_to achieves_path, notice: "更新されました！"
+		else
+			render action: "edit"
+		end
+
+	end
+
+	def destroy
+		@achieve = Achieve.find(params[:id])
+		@achieve.destroy
+		redirect_to achieves_path, notice:"削除されたよん！"
+	end
+
 end
